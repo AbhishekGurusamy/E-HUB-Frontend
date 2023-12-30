8a080c055b2fd95b85a0fe4545ab260c2339071f
Abhishek G <abhiguru.inf@gmail.com>
2023-12-30 16:01:58 +0530

    authentication folder added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LogInComponent } from './log-in/log-in.component';
+import { LogInComponent } from './authentication/log-in/log-in.component';
 
 @NgModule({
   declarations: [

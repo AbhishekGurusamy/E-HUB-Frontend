cdb8d46206ba99dfa26b06ca1b680ea6b9c84c65
uif39101 <gowtham.s-EXT@continental-corporation.com>
2023-12-31 21:08:42 +0530

    auth guard init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StoreComponent } from './store/store.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuardGuard } from './guards/auth-guard.guard';
 
 const routes: Routes = [
-  {path:'createstore',component:StoreComponent},
+  {path:'createstore',component:StoreComponent, canActivate:[AuthGuardGuard]},
   {path:'dashboard',component:DashboardComponent},
   {path:'',redirectTo:'dashboard',pathMatch: 'full'}
 ];

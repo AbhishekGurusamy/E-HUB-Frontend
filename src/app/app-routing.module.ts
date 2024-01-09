206ae5f22473ff1897c695e6b88b022d04f9ad83
Abhishek G <abhiguru.inf@gmail.com>
2024-01-09 23:34:01 +0530

    testing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StoreComponent } from './store/store.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { AuthGuardGuard } from './guards/auth-guard.guard';
+import { auth_guard } from './guards/auth-guard.guard';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path:'createstore',component:StoreComponent, canActivate:[AuthGuardGuard]},
-  {path:'dashboard',component:DashboardComponent},
-  {path:'register',component:RegisterComponent},
-  {path:'login',component:LoginComponent},
-  {path:'',redirectTo:'dashboard',pathMatch: 'full'}
+  { path: 'createstore', component: StoreComponent, canActivate: [auth_guard] },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/guards/auth-guard.guard.ts
+++ src/app/guards/auth-guard.guard.ts
@@ -1,29 +1,45 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router, createUrlTreeFromSnapshot } from '@angular/router';
+import { inject } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, createUrlTreeFromSnapshot, CanActivateFn } from '@angular/router';
 import { Observable } from 'rxjs';
 import { ToastService } from 'src/app/services/toast.service'
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthGuardGuard implements CanActivate {
-  constructor(private router: Router, private toast:ToastService) { }
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class AuthGuardGuard implements CanActivate {
+//   constructor(private router: Router, private toast:ToastService) { }
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    // this.router.navigate(['/somepage'])
-    if (localStorage.getItem('token')) {
-      this.toast.showsuccess('Login Successful')
-      // console.log(state.url)
-      return createUrlTreeFromSnapshot(route, [state.url]);
-    }
-    else {
-      this.toast.showerror('User not logged in')
-      // this.router.navigate(['/register'])
-      return createUrlTreeFromSnapshot(route, ['login']);
-    }
+//   canActivate(
+//     route: ActivatedRouteSnapshot,
+//     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+//     // this.router.navigate(['/somepage'])
+//     if (localStorage.getItem('token')) {
+//       this.toast.showsuccess('Login Successful')
+//       // console.log(state.url)
+//       return createUrlTreeFromSnapshot(route, [state.url]);
+//     }
+//     else {
+//       this.toast.showerror('User not logged in')
+//       // this.router.navigate(['/register'])
+//       return createUrlTreeFromSnapshot(route, ['login']);
+//     }
 
+//   }
+
+// }
+
+export const auth_guard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+  Observable<UrlTree> | Promise<UrlTree> | UrlTree => {
+  const toast = inject(ToastService)
+  if (localStorage.getItem('token')) {
+    toast.showsuccess('Login Successful')
+    // console.log(state.url)
+    return createUrlTreeFromSnapshot(route, [state.url]);
   }
-
+  else {
+    toast.showerror('User not logged in')
+    // this.router.navigate(['/register'])
+    console.log(createUrlTreeFromSnapshot(route, ['/login']))
+    return createUrlTreeFromSnapshot(route, [state.url]);
+  }
 }
